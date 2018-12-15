55f3a7efa96a4051c71f83b74be2d13775a4d663
Anthony D <dunahee@gmail.com>
2018-12-15 10:51:18 -0600

    rounded numbers for easier quick check

--- client.test.js
+++ client.test.js
@@ -2,9 +2,9 @@ console.log('Test code should create dummy employee and print them to table alon
 
 function testCode(){
 //creating test employees
-const anthony = new Employee('Anthony', 'Dunahee', 4669, 'Aspiring Developer', 23456);
-const peter = new Employee('Peter', 'Parker', 2345, 'Delivery Person', 34567);
-const wonder = new Employee('Wonder', 'Woman', 1234, 'Superheroine', 12345);
+const anthony = new Employee('Anthony', 'Dunahee', 4669, 'Aspiring Developer', 12000);
+const peter = new Employee('Peter', 'Parker', 2345, 'Delivery Person', 36000);
+const wonder = new Employee('Wonder', 'Woman', 1234, 'Superheroine', 48000);
 
 //filling employee array
 employees.push(anthony);
